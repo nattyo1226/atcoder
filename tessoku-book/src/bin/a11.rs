aa198dad1db913718a7ef9621ae98041e476c7bb
nattyo1226 <132973057+nattyo1226@users.noreply.github.com>
2024-03-08 04:29:59 +0900

    tessoku-book up to 3.1

--- tessoku-book/src/bin/a11.rs
+++ tessoku-book/src/bin/a11.rs
@@ -1,3 +1,24 @@
+use proconio::{input, fastout};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        x: usize,
+        a: [usize; n],
+    }
+    let mut l = 0;
+    let mut r = a.len() - 1;
+    let mut m;
+    loop {
+        m = (l + r) / 2;
+        if x < a[m] {
+            r = m - 1;
+        } else if x == a[m] {
+            break;
+        } else {
+            l = m + 1;
+        }
+    }
+    println!("{:?}", m + 1);
 }
--- tessoku-book/src/bin/b11.rs
+++ tessoku-book/src/bin/b11.rs
@@ -1,3 +1,17 @@
+use proconio::{input, fastout};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut a: [usize; n],
+        q: usize,
+        x: [usize; q],
+    }
+    a.sort();
+    let mut r;
+    for &i in &x {
+        r = a.partition_point(|&y| y < i);
+        println!("{}", r);
+    }
 }
